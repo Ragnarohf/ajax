7fc4855c5281a0a21ce89932eec99249d5ae937f
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-15 13:47:51 +0200

    mail

--- 3-cours/connexion.php
+++ 3-cours/connexion.php
@@ -6,16 +6,19 @@ $pdo = new PDO('mysql:host=localhost;dbname=vinyl_shop;charset=utf8', 'root', ''
     PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
 ));
 $error = null;
-$email = $_GET['email'];
+
 
 
 try {
+    $email = $_GET['email'];
     $statement = $pdo->query("SELECT * from user where email = '$email'");
     $user = $statement->fetch();
-    var_dump($user);
-} catch (PDOException $exe) {
-    $error = $exe->getMessage();
+    // var_dump($user);
+    print_r($user);
+} catch (PDOException $e) {
+    $error = $e->getMessage();
 }
+
 // if (!empty($_GET)) {
 //     echo "<h1>je suis bien connecté $_GET[email]</h1>";
 // }
