587141e3963910e3b991aef48c86a10db0694fc6
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-15 11:21:38 +0200

    message

--- 3-cours/app.js
+++ 3-cours/app.js
@@ -1,1 +1,12 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  $(".userConnexion").submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: "GET",
+      url: "connexion.php",
+      data: "data",
+      dataType: "dataType",
+      success: function (response) {},
+    });
+  });
+});
