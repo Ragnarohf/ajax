cb4ce8a7dceadc44caa49bdf3f71a970103b7bcb
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-15 14:23:18 +0200

    correctif

--- 3-cours/connexion.php
+++ 3-cours/connexion.php
@@ -1,25 +1,30 @@
 <?php
-// var_dump($_GET);
-
-$pdo = new PDO('mysql:host=localhost;dbname=vinyl_shop;charset=utf8', 'root', '', array(
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
-));
+//var_dump($_GET);
+$pdo = new PDO("mysql:host=localhost;dbname=vinyl_shop;charset=utf8", 'root', '', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
 $error = null;
-
-
-
 try {
     $email = $_GET['email'];
-    $statement = $pdo->query("SELECT * from user where email = '$email'");
-    var_dump($statement);
+
+
+    $statement = $pdo->prepare("SELECT * from user where email = :email ");
+    $statement->execute([
+        'email' => $email
+    ]);
     $user = $statement->fetch();
-    var_dump($user);
-    print_r($user);
+    var_dump($user['pwd']);
+    var_dump(password_verify($_GET['password'], $user['pwd']));
+    if (password_verify($_GET['password'], $user['pwd'])) {
+        echo "welcome in your profil";
+    } else {
+        echo "erreur de connexion";
+    }
+    // 
+    // print_r($user);
+
+
 } catch (PDOException $e) {
     $error = $e->getMessage();
 }
-
-// if (!empty($_GET)) {
-//     echo "<h1>je suis bien connecté $_GET[email]</h1>";
-// }
