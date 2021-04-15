1b352f303733b8872f83caa1ae6391d4167bf224
Ragnarohf <mathieuthoumire@hotmail.fr>
2021-04-15 16:32:49 +0200

    resultat

--- 4-cours/app.js
+++ 4-cours/app.js
@@ -1,1 +1,28 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  myArr = [];
+  $.ajax({
+    type: "GET",
+    url: "states.xml",
+    // data: "data",
+    dataType: "xml",
+    success: function (response) {
+      // console.log(response);
+      $(response)
+        .find("state")
+        .each(function () {
+          myArr.push($(this).attr("label"));
+        });
+      console.log(myArr);
+    },
+    complete: function () {
+      $("input.state").autocomplete({
+        source: myArr,
+        minLength: 1,
+        select: function (ui) {
+          $("input.state").val(ui.item.value);
+          $("formRecherche").submit();
+        },
+      });
+    },
+  });
+});
